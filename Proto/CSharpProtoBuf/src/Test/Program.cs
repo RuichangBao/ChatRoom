745b33b364f664c8de94a41f044053a502132451
RuichangBao <1294838346@qq.com>
2024-07-22 20:53:28 +0800

    proot

---
+++ Proto/CSharpProtoBuf/src/Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using Google.Protobuf.Reflection;
+
+namespace Google.Protobuf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessage imessage = new TestRequest();
+            
+            byte[]data = imessage.ToByteArray();
+            CodedOutputStream output = new CodedOutputStream(data);
+            imessage.WriteTo(output);
+            output.WriteRawMessage(imessage);
+            CodedOutputStream.ComputeStringSize("");
+            CodedOutputStream.ComputeUInt32Size(100);
+        }
+    }
+    public class TestRequest : IMessage
+    {
+        public MessageDescriptor Descriptor => throw new NotImplementedException();
+
+        public int CalculateSize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MergeFrom(CodedInputStream input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteTo(CodedOutputStream output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
